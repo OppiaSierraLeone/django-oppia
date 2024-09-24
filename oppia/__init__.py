6adb4980cf8fe42cb618b4d8ae51c77418364bbe
hakimks <hakimks@gmail.com>
2024-09-24 09:28:08 +0300

    change version number

--- oppia/__init__.py
+++ oppia/__init__.py
@@ -1,9 +1,5 @@
 import warnings
 
-<<<<<<< HEAD
 VERSION = (0, 15, 3, 'sl', '1', 'release')
-=======
-VERSION = (0, 15, 3, 'core', '1', 'release')
->>>>>>> upstream/release-0-15-3
 DEFAULT_IP_ADDRESS = None
 warnings.simplefilter('default')
